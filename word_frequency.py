ff2cfa2e1f1d288b09f1068b9f4f38974ba70e51
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-05-30 14:25:39 +0100

    chore: Refactor clean_text function in word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -4,12 +4,12 @@ def read_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read()
 
-def clean_text(text): # Added missing 'def' keyword
+def clean_text(text):
     text = text.lower()
     text = text.translate(str.maketrans('', '', string.punctuation))
     return text
 
 myFile = read_file('./a_LittleMaid.txt')
 
-clean = clean_text(myFile)  # Indented the code block properly
+clean = clean_text(myFile)
 print(clean[:1000])
