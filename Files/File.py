f85dab0c419c4d4ddbd4c6fd1fa88ccf2495f1c8
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-06-01 20:55:34 +0100

    practise copying a file

--- Files/File.py
+++ Files/File.py
@@ -4,4 +4,19 @@ with open(r'c:\Users\user\Downloads\Python_practise\Files\sample.txt', 'r', enco
     
 # Write to a File
 with open(r'c:\Users\user\Downloads\Python_practise\Files\output.txt', 'w', encoding= 'utf-8') as file:
-    file.write('Yaay! I successfully used the write mode. the file name provided was not existent so It created a new file')
+    file.write('Yaay! I successfully used the write mode.\nThe file name provided was not existent so It created a new file')
+    
+# Append to a File
+with open(r'c:\Users\user\Downloads\Python_practise\Files\output.txt', 'a', encoding= 'utf-8') as file:
+    file.write('\nWell, see who just used the append mode. Yippy!')
+    
+# Read a File Line by Line
+with open(r'c:\Users\user\Downloads\Python_practise\Files\output.txt', 'r', encoding= 'utf-8') as file:
+    for line in file:
+        print(line, end='')
+        
+# Copy File.
+with open(r'c:\Users\user\Downloads\Python_practise\Files\output.txt', 'r', encoding='utf-8') as file:
+    with open(r'c:\Users\user\Downloads\Python_practise\Files\copy.txt', 'w', encoding= 'utf-8') as file2:
+        for line in file:
+            file2.write(line)
