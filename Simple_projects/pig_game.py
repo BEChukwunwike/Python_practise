297e7089f1501ecc58784ff59ae1a4d75880cfad
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-06-06 18:16:31 +0100

    feat: Add player names and scores to pig game

--- Simple_projects/pig_game.py
+++ Simple_projects/pig_game.py
@@ -3,20 +3,31 @@ import random
 def roll():
     return random.randint(1, 6)
 
+player_dict = {}
+
+
 def main():
     print("Welcome to the Pig Game")
     while True:
         try:
             players = int(input("How many players? \nChoose 2 - 4: "))
-        except:
+        except ValueError:
             print("Enter numbers between 2 - 4")
             continue
-        if players in range(2, 4):
+        if players in range(2, 5):
             break
         else:
             print("Enter numbers between 2 - 4")
             continue
         
+    players_scores = {}
+    for i in range(1, players + 1):
+        player_name = input(f"Enter the name for player {i}: ")
+        players_scores[player_name] = 0
+    
+        
+    print(players_scores)
+    
         
             
     
