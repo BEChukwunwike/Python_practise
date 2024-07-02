6859f34185cb81163f7c88fc87a3d5010ccde1f5
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-07-02 19:57:14 +0100

    feat: Add greet view to hello app
    This commit adds a new view called `greet` to the `hello` app in the `mydjango` project. The `greet` view takes a `name` parameter and renders the `index.html` template with the capitalized `name` passed as context.

--- django_practise/CS50W/mydjango/hello/views.py
+++ django_practise/CS50W/mydjango/hello/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request, name=None):
-    if name:
-        name = name.capitalize()
+
+def index(request):
+    return render(request, 'Hello/index.html')
+
+def greet(request, name):
+    name = name.capitalize()
     return render(request, 'Hello/index.html',{
         'name': name
     })
