ae045c7899a7d00eb732298ec2dd6d3c69615f9a
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-07-03 16:15:57 +0100

    refactor: Update static file configuration in settings.py
    This commit updates the `STATICFILES_DIRS` and `STATIC_ROOT` settings in the `settings.py` file of the `mydjango` project. The `STATICFILES_DIRS` list is modified to include the `static` directory, and the `STATIC_ROOT` variable is set to the `staticfiles` directory. This ensures that static files are collected correctly during deployment.

--- django_practise/CS50W/mydjango/mydjango/urls.py
+++ django_practise/CS50W/mydjango/mydjango/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -24,3 +26,5 @@ urlpatterns = [
     path('hello/', include('hello.urls')),
     path('newyear/', include('newyear.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
