17cfeb66d403cc36652e76c9f77d52e93e532832
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-07-05 13:34:09 +0100

    "Updated index.html template and views.py to render tasks list"

--- django_practise/CS50W/mydjango/tasks/views.py
+++ django_practise/CS50W/mydjango/tasks/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+tasks = ['Cook', 'Read', 'Sleep']
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+    
+def add(request):
+    return render(request, 'task/add.html')
