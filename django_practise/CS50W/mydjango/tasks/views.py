1b8cceddabce6af0d0f2f3ff13ab3c1496cba8fd
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-07-06 23:10:12 +0100

    feat: Add error handling for empty task list in index.html

--- django_practise/CS50W/mydjango/tasks/views.py
+++ django_practise/CS50W/mydjango/tasks/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django import forms
-
-tasks = ['Cook', 'Read', 'Sleep']
+from django.http import HttpResponseRedirect
 
 class AddTaskForm(forms.Form):
     task = forms.CharField(label="Add new task here")
     
 # Create your views here.
 def index(request):
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session['tasks']
     })
     
 def add(request):
+    if request.method == 'POST':
+        form = AddTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session['tasks'] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, 'tasks/add.html', {
+                'form': form
+            })
+            
     return render(request, 'tasks/add.html', {
         'form': AddTaskForm()
     })
